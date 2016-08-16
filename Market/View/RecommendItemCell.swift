849335d452fa4ce591319aa04e9900b609871490
flomym <i13moriwaki@gmail.com>
2016-08-16 12:55:26 +0900

    おすすめ表示

--- Market/View/RecommendItemCell.swift
+++ Market/View/RecommendItemCell.swift
@@ -6,5 +6,12 @@ class RecommendItemCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     
     @IBOutlet weak var priceLabel: UILabel!
+    
     @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func update(withItem item: Item){
+        nameLabel.text = item.name
+        priceLabel.text = "\(item.price)円"
+        descriptionLabel.text = item.desc
+    }
 }
---
+++ Model/Item.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Item {
+
+    let id: Int
+    let name: String
+    let desc: String
+    let price: Int
+    let imageURL: NSURL     // nilが入らない
+
+    
+}
