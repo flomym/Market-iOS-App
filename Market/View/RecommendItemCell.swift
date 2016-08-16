1b2b6e84986685a69fd64a761bdc4d4bd8239123
flomym <i13moriwaki@gmail.com>
2016-08-16 11:26:16 +0900

    labelなどの要素の配置指定

---
+++ Market/View/RecommendItemCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class RecommendItemCell: UITableViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
