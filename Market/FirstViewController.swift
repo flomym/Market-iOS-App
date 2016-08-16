58cf75ca9426c1d47a3df82a5e27b7a6a13467cb
flomym <i13moriwaki@gmail.com>
2016-08-16 09:48:44 +0900

    Initial Commit

---
+++ Market/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Market
+//
+//  Created by tomoka-moriwaki on 2016/08/16.
+//  Copyright © 2016年 tomoka-moriwaki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
